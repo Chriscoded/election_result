62e4b05878f6cac5093122224ba59792bc724a41
chriscoded <amaemechris@gmail.com>
2022-11-09 11:52:24 +0100

    first commit

---
+++ app/Models/Announced_pu_Result.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Announced_pu_Result extends Model
+{
+    use HasFactory;
+
+    protected $table = 'announced_pu_results';
+
+    protected $primaryKey = 'uniqueid';
+    public $timestamps = false;
+
+    protected $fillable = ['polling_unit_uniqueid',
+    'party_abbreviation',
+    'party_score',
+    'entered_by_user',
+    'date_entered',
+    'user_ip_address'];
+
+
+    protected $hidden = [''];
+
+    protected $visible = [''];
+
+    
+}
---
+++ app/Models/Ward.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ward extends Model
+{
+    use HasFactory;
+    protected $table = 'ward';
+
+    protected $primaryKey = 'uniqueid';
+
+    public $timestamps = true;
+
+    protected $fillable = ['polling_unit_id' ,'ward_id','lga_id','uniquewardid','polling_unit_number','polling_unit_name','polling_unit_description','lat','long','entered_by_user','date_entered','user_ip_address'];
+
+    protected $hidden = [''];
+
+    protected $visible = [''];
+
+    public function Lga()
+    {
+        return $this->hasOne(Lga::class, 'uniqueid', 'lga_id')->latest();
+            // ->limit(6)
+            // ->get();
+    }
+
+    public function polling_units()
+    {
+        return $this->hasMany(Polling_Unit::class, 'ward_id', 'uniqueid')->latest();
+            // ->limit(6)
+            // ->get();
+    }
+}
